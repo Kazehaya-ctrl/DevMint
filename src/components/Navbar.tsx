8a89d4c2499cf161a65d93c6e86bbc869e25ca4c
Kazehaya-ctrl <mahitomaki96@gmail.com>
2025-02-26 00:42:35 +0530

    upt: scroll navigations

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,130 +4,145 @@ import { MoreVertical, X } from "lucide-react";
 import { ContactDialog } from "./ContactDialogueBox";
 
 export default function Navbar() {
-  const [isScrolled, setIsScrolled] = useState(false);
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const [isContactDialogOpen, setIsContactDialogOpen] = useState(false);
+	const [isScrolled, setIsScrolled] = useState(false);
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+	const [isContactDialogOpen, setIsContactDialogOpen] = useState(false);
 
-  
+	useEffect(() => {
+		const handleScroll = () => {
+			setIsScrolled(window.scrollY > 50);
+		};
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 50);
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+	const handleContactClick = (e: any) => {
+		e.preventDefault();
+		setIsContactDialogOpen(true);
+		setIsMobileMenuOpen(false);
+	};
 
-  const handleContactClick = (e: any) => {
-    e.preventDefault();
-    setIsContactDialogOpen(true);
-    setIsMobileMenuOpen(false);
-  };
+	const handleScroll = (id: string) => {
+		const element = document.getElementById(id);
+		if (element) {
+			element.scrollIntoView({ behavior: "smooth" });
+		}
+	};
 
-  const handleScroll = (id: string) => {
-    const element = document.getElementById(id)
-    if (element) {
-      element.scrollIntoView({behavior: 'smooth'})
-    }
-  }
+	const menuItems = [
+		{ text: "Home", onClick: () => handleScroll("home") },
+		{ text: "About", onClick: () => handleScroll("about") },
+		{ text: "Portfolio", onClick: () => handleScroll("portfolio") },
+		{ text: "Contact", onClick: handleContactClick },
+		{ text: "FAQ", href: "#" },
+	];
 
-  const menuItems = [
-    { text: "Home", onClick: () => handleScroll('home') },
-    { text: "About", onClick: () => handleScroll('about') },
-    { text: "Portfolio", onClick: () => handleScroll('portfolio')},
-    { text: "Contact", onClick: handleContactClick },
-    { text: "FAQ", href: "#" }
-  ];
+	const mobileMenuVariants = {
+		closed: {
+			opacity: 0,
+			y: -20,
+			display: "none",
+		},
+		open: {
+			opacity: 1,
+			y: 0,
+			display: "block",
+		},
+	};
 
-  const mobileMenuVariants = {
-    closed: {
-      opacity: 0,
-      y: -20,
-      display: "none"
-    },
-    open: {
-      opacity: 1,
-      y: 0,
-      display: "block"
-    }
-  };
+	return (
+		<>
+			<motion.nav
+				initial={{ opacity: 0, y: -20 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.8, ease: "easeOut" }}
+				className={`fixed top-0 left-0 right-0 mx-auto z-50 transition-all duration-500 ease-in-out ${
+					isScrolled
+						? "w-[80%] mt-4 rounded-2xl container"
+						: "w-full rounded-none"
+				} bg-black/30 backdrop-blur-md text-black h-20 md:h-28 px-8 border-double border-b-slate-700 shadow-lg`}
+			>
+				<div className="max-w-7xl mx-auto h-full flex items-center justify-between">
+					<motion.div
+						whileHover={{ scale: 1.1, rotate: 3 }}
+						className="flex items-center gap-x-4 transition"
+					>
+						<div className="backdrop-blur-md rounded-2xl">
+							<img src="./cube.png" width={50} alt="Logo" />
+						</div>
+						<div className="text-xl text-white font-semibold text-center">
+							Devmint
+						</div>
+					</motion.div>
 
-  return (
-    <>
-      <motion.nav
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, ease: "easeOut" }}
-        className={`fixed top-0 left-0 right-0 mx-auto z-50 transition-all duration-500 ease-in-out ${
-          isScrolled ? 'w-[80%] mt-4 rounded-2xl container' : 'w-full rounded-none'
-        } bg-black/30 backdrop-blur-md text-black h-20 md:h-28 px-8 border-double border-b-slate-700 shadow-lg`}
-      >
-        <div className="max-w-7xl mx-auto h-full flex items-center justify-between">
-          <motion.div whileHover={{ scale: 1.1, rotate: 3 }} className="flex items-center gap-x-4 transition">
-            <div className="backdrop-blur-md rounded-2xl">
-              <img src="./cube.png" width={50} alt="Logo" />
-            </div>
-            <div className="text-xl text-white font-semibold text-center">Devmint</div>
-          </motion.div>
+					{/* Desktop Menu */}
+					<div className="hidden md:flex items-center">
+						<div className="flex space-x-6 text-white text-md">
+							{menuItems.map((item, index) => (
+								<motion.a
+									key={index}
+									href={item.href}
+									onClick={item.onClick}
+									whileHover={{ scale: 1.3, color: "#ccc" }}
+									transition={{
+										type: "spring",
+										stiffness: 300,
+									}}
+									className="hover:text-gray-500 transition"
+								>
+									{item.text}
+								</motion.a>
+							))}
+						</div>
+					</div>
 
-          {/* Desktop Menu */}
-          <div className="hidden md:flex items-center">
-            <div className="flex space-x-6 text-white text-md">
-              {menuItems.map((item, index) => (
-                <motion.a
-                  key={index}
-                  href={item.href}
-                  onClick={item.onClick}
-                  whileHover={{ scale: 1.3, color: "#ccc" }}
-                  transition={{ type: "spring", stiffness: 300 }}
-                  className="hover:text-gray-500 transition"
-                >
-                  {item.text}
-                </motion.a>
-              ))}
-            </div>
-          </div>
+					{/* Mobile Menu Button */}
+					<div className="md:hidden">
+						<motion.button
+							whileTap={{ scale: 0.95 }}
+							onClick={() =>
+								setIsMobileMenuOpen(!isMobileMenuOpen)
+							}
+							className="text-white p-2"
+						>
+							{isMobileMenuOpen ? (
+								<X size={24} />
+							) : (
+								<MoreVertical size={24} />
+							)}
+						</motion.button>
+					</div>
+				</div>
+			</motion.nav>
 
-          {/* Mobile Menu Button */}
-          <div className="md:hidden">
-            <motion.button
-              whileTap={{ scale: 0.95 }}
-              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-              className="text-white p-2"
-            >
-              {isMobileMenuOpen ? <X size={24} /> : <MoreVertical size={24} />}
-            </motion.button>
-          </div>
-        </div>
-      </motion.nav>
+			{/* Mobile Menu Dropdown */}
+			<motion.div
+				initial="closed"
+				animate={isMobileMenuOpen ? "open" : "closed"}
+				variants={mobileMenuVariants}
+				transition={{ duration: 0.3 }}
+				className="fixed top-20 left-0 right-0 bg-black/90 backdrop-blur-md md:hidden z-40 p-4"
+			>
+				<div className="flex flex-col space-y-4">
+					{menuItems.map((item, index) => (
+						<motion.a
+							key={index}
+							href={item.href}
+							onClick={item.onClick}
+							whileHover={{ scale: 1.05 }}
+							className="text-white text-lg text-center py-2 hover:bg-white/10 rounded-lg"
+						>
+							{item.text}
+						</motion.a>
+					))}
+				</div>
+			</motion.div>
 
-      {/* Mobile Menu Dropdown */}
-      <motion.div
-        initial="closed"
-        animate={isMobileMenuOpen ? "open" : "closed"}
-        variants={mobileMenuVariants}
-        transition={{ duration: 0.3 }}
-        className="fixed top-20 left-0 right-0 bg-black/90 backdrop-blur-md md:hidden z-40 p-4"
-      >
-        <div className="flex flex-col space-y-4">
-          {menuItems.map((item, index) => (
-            <motion.a
-              key={index}
-              href={item.href}
-              onClick={item.onClick}
-              whileHover={{ scale: 1.05 }}
-              className="text-white text-lg text-center py-2 hover:bg-white/10 rounded-lg"
-            >
-              {item.text}
-            </motion.a>
-          ))}
-        </div>
-      </motion.div>
-
-      {/* Contact Dialog */}
-      <ContactDialog
-        isOpen={isContactDialogOpen} 
-        onClose={() => setIsContactDialogOpen(false)} 
-      />
-    </>
-  )};
+			{/* Contact Dialog */}
+			<ContactDialog
+				isOpen={isContactDialogOpen}
+				onClose={() => setIsContactDialogOpen(false)}
+			/>
+		</>
+	);
+}
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,88 +1,116 @@
-import { ReactTyped } from 'react-typed';
-import { motion } from 'framer-motion';
-import { Spotlight } from '../components/Spotlight';
+import { ReactTyped } from "react-typed";
+import { motion } from "framer-motion";
+import { Spotlight } from "../components/Spotlight";
 
 export default function Landing() {
-    return (
-      <div className="min-h-screen w-full flex items-center justify-center bg-[#000000] bg-[linear-gradient(to_right,#ffffff15_2px,transparent_2px),linear-gradient(to_bottom,#ffffff15_2px,transparent_2px)] [background-size:50px_40px]">
-        <Spotlight/>
-            <div className="flex flex-col md:flex-row items-center justify-center w-full max-w-7xl px-4 md:px-0">
-                
-                {/* Left Section */}
-                <motion.div 
-                    initial={{ opacity: 0, x: -100 }} 
-                    animate={{ opacity: 1, x: 0 }} 
-                    transition={{ duration: 0.8, ease: 'easeOut' }}
-                    className="w-full md:w-1/2 space-y-6 text-center md:text-left"
-                >
-                    <motion.div 
-                        initial={{ opacity: 0, y: -20 }} 
-                        animate={{ opacity: 1, y: 0 }} 
-                        transition={{ duration: 0.6, delay: 0.3 }}
-                        className="flex items-center backdrop-blur-md bg-white/30 p-1.5 mx-auto md:mx-0 rounded-lg w-fit shadow-lg"
-                    >
-                        <div className="bg-blue-700 text-xs text-white font-bold px-3 py-1 rounded-md">
-                            NEW
-                        </div>
-                        <div className="bg-opacity-20 text-xs px-3 py-1 text-white rounded-r-md">
-                            Innovation Solution
-                        </div>
-                    </motion.div>
-                    
-                    <h1 className="text-4xl md:text-6xl text-white font-bold leading-tight">
-                        Powering Your Creative <br /> Dreams, Embrace <br />
-                        <ReactTyped className='text-blue-600' strings={["DEVMINT."]} typeSpeed={120} backSpeed={140} loop/>
-                    </h1>
-                    
-                    <motion.p 
-                        initial={{ opacity: 0, y: 20 }} 
-                        animate={{ opacity: 1, y: 0 }} 
-                        transition={{ duration: 0.6, delay: 0.5 }}
-                        className="text-gray-400 max-w-md mx-auto md:mx-0"
-                    >
-                        From Software Development to Design Services, we deliver tailored solutions for every visionary idea using cutting-edge technology.
-                    </motion.p>
-                    
-                    <motion.div 
-                        initial={{ opacity: 0, scale: 0.9 }} 
-                        animate={{ opacity: 1, scale: 1 }} 
-                        transition={{ duration: 0.5, delay: 0.7 }}
-                        className="flex flex-col sm:flex-row gap-4 pt-4 justify-center md:justify-start"
-                    >
-                        <button className="bg-white text-black px-6 py-3 text-sm rounded-lg hover:bg-gray-200 hover:scale-105 transition-transform">
-                            Explore Work
-                        </button>
-                        <button className="bg-white/30 text-white backdrop-blur-md text-sm px-6 py-3 rounded-lg hover:bg-opacity-20 hover:scale-105 transition-transform">
-                            What is Devmint?
-                        </button>
-                    </motion.div>
-                </motion.div>
-                
-                {/* Right Section - Cube Image */}
-                <motion.div 
-                    initial={{ opacity: 0, x: 100 }} 
-                    animate={{ opacity: 1, x: 0 }} 
-                    transition={{ duration: 0.8, ease: 'easeOut' }}
-                    className="w-full md:w-1/2 flex justify-center items-center relative mt-10 md:mt-0 order-last md:order-none"
-                >
-                    <div className="absolute w-full h-full flex justify-center">
-                        <div className="absolute inset-0 w-[80%] h-[80%] md:w-full md:h-full bg-gradient-radial 
-                                        from-transparent via-blue-500 to-purple-500 
-                                        blur-[80px] opacity-80 mask-image-radial">
-                        </div>
-                    </div>
+	const handleClick = (id: string) => {
+		const element = document.getElementById(id);
+		if (element) {
+			element.scrollIntoView({ behavior: "smooth" });
+		}
+	};
+	return (
+		<div className="min-h-screen w-full flex items-center justify-center bg-[#000000] bg-[linear-gradient(to_right,#ffffff15_2px,transparent_2px),linear-gradient(to_bottom,#ffffff15_2px,transparent_2px)] [background-size:50px_40px]">
+			<Spotlight />
+			<div className="flex flex-col md:flex-row items-center justify-center w-full max-w-7xl px-4 md:px-0">
+				{/* Left Section */}
+				<motion.div
+					initial={{ opacity: 0, x: -100 }}
+					animate={{ opacity: 1, x: 0 }}
+					transition={{ duration: 0.8, ease: "easeOut" }}
+					className="w-full md:w-1/2 space-y-6 text-center md:text-left"
+				>
+					<motion.div
+						initial={{ opacity: 0, y: -20 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{ duration: 0.6, delay: 0.3 }}
+						className="flex items-center backdrop-blur-md bg-white/30 p-1.5 mx-auto md:mx-0 rounded-lg w-fit shadow-lg"
+					>
+						<div className="bg-blue-700 text-xs text-white font-bold px-3 py-1 rounded-md">
+							NEW
+						</div>
+						<div className="bg-opacity-20 text-xs px-3 py-1 text-white rounded-r-md">
+							Innovation Solution
+						</div>
+					</motion.div>
 
-                    <motion.div
-                        animate={{ y: [0, -20, 0] }}
-                        transition={{ duration: 2, repeat: Infinity, ease: "easeInOut" }}
-                        className="relative z-10 flex justify-center"
-                    >
-                        <img src="./cube.png" alt="" className='relative z-10 w-[60%] md:w-[80%] object-contain 
+					<h1 className="text-4xl md:text-6xl text-white font-bold leading-tight">
+						Powering Your Creative <br /> Dreams, Embrace <br />
+						<ReactTyped
+							className="text-blue-600"
+							strings={["DEVMINT."]}
+							typeSpeed={120}
+							backSpeed={140}
+							loop
+						/>
+					</h1>
+
+					<motion.p
+						initial={{ opacity: 0, y: 20 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{ duration: 0.6, delay: 0.5 }}
+						className="text-gray-400 max-w-md mx-auto md:mx-0"
+					>
+						From Software Development to Design Services, we deliver
+						tailored solutions for every visionary idea using
+						cutting-edge technology.
+					</motion.p>
+
+					<motion.div
+						initial={{ opacity: 0, scale: 0.9 }}
+						animate={{ opacity: 1, scale: 1 }}
+						transition={{ duration: 0.5, delay: 0.7 }}
+						className="flex flex-col sm:flex-row gap-4 pt-4 justify-center md:justify-start"
+					>
+						<button
+							onClick={() => handleClick("portfolio")}
+							className="bg-white text-black px-6 py-3 text-sm rounded-lg hover:bg-gray-200 hover:scale-105 transition-transform"
+						>
+							Explore Work
+						</button>
+						<button
+							onClick={() => handleClick("services")}
+							className="bg-white/30 text-white backdrop-blur-md text-sm px-6 py-3 rounded-lg hover:bg-opacity-20 hover:scale-105 transition-transform"
+						>
+							What is Devmint?
+						</button>
+					</motion.div>
+				</motion.div>
+
+				{/* Right Section - Cube Image */}
+				<motion.div
+					initial={{ opacity: 0, x: 100 }}
+					animate={{ opacity: 1, x: 0 }}
+					transition={{ duration: 0.8, ease: "easeOut" }}
+					className="w-full md:w-1/2 flex justify-center items-center relative mt-10 md:mt-0 order-last md:order-none"
+				>
+					<div className="absolute w-full h-full flex justify-center">
+						<div
+							className="absolute inset-0 w-[80%] h-[80%] md:w-full md:h-full bg-gradient-radial 
+                                        from-transparent via-blue-500 to-purple-500 
+                                        blur-[80px] opacity-80 mask-image-radial"
+						></div>
+					</div>
+
+					<motion.div
+						animate={{ y: [0, -20, 0] }}
+						transition={{
+							duration: 2,
+							repeat: Infinity,
+							ease: "easeInOut",
+						}}
+						className="relative z-10 flex justify-center"
+					>
+						<img
+							src="./cube.png"
+							alt=""
+							className="relative z-10 w-[60%] md:w-[80%] object-contain 
                                             drop-shadow-[0_0_40px_rgba(0,65,225,0.8)] 
-                                            transition-transform duration-300 hover:scale-105'/>
-                    </motion.div>
-                </motion.div>
-            </div>
-        </div>
-    );
+                                            transition-transform duration-300 hover:scale-105"
+						/>
+					</motion.div>
+				</motion.div>
+			</div>
+		</div>
+	);
 }
